80665a8717705078ca51e585f7bed6309963a190
Daniel Alcala Fernandez <daniel.alcfer@educa.jcyl.es>
2020-01-29 10:17:44 +0100

    29/01/2020 WSRest

--- controller/cWSRest.php
+++ controller/cWSRest.php
@@ -10,10 +10,19 @@ if(isset($_REQUEST["calcular"])){
 }
 if($entrada){
     $ch= curl_init();
-    curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/dataset/cordisH2020projects");
+    //curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/dataset/cordisH2020projects.rdf");
+    curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/apiodp/action/package_list");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    $array=["limit"=>20,"offset"=>3];
+    $json= json_encode($array);
+    //curl_setopt($ch, CURLOPT_PROTOCOLS, $json);
     $resultado=curl_exec($ch);
     curl_close($ch);
+    //$xml= new SimpleXMLElement($resultado);
+    /*$rdf=$xml->RDF->Description->name;
+    echo $rdf;*/
+    //echo $xml;
     echo $resultado;
     
 }else{
