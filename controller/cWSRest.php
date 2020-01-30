f581bd847fd64dc6ec89b89e130920d46d283fdc
Daniel Alcala Fernandez <daniel.alcfer@educa.jcyl.es>
2020-01-30 10:10:29 +0100

    30/01/2020 WSRest pagina html

--- controller/cWSRest.php
+++ controller/cWSRest.php
@@ -2,7 +2,7 @@
 if(!isset($_SESSION[USUARIOA])){
     header('Location: Layout.php');
 }
-if(isset($_REQUEST["calcular"])){
+if(isset($_REQUEST["ids"])){
  $entrada=true;
  $aErrores=[];
 }else{
@@ -10,21 +10,32 @@ if(isset($_REQUEST["calcular"])){
 }
 if($entrada){
     $ch= curl_init();
-    //curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/dataset/cordisH2020projects.rdf");
     curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/apiodp/action/package_list");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, TRUE);
     $array=["limit"=>20,"offset"=>3];
-    $json= json_encode($array);
-    //curl_setopt($ch, CURLOPT_PROTOCOLS, $json);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $array);
     $resultado=curl_exec($ch);
     curl_close($ch);
-    //$xml= new SimpleXMLElement($resultado);
-    /*$rdf=$xml->RDF->Description->name;
-    echo $rdf;*/
-    //echo $xml;
-    echo $resultado;
+    $_SESSION["resultadoAPI"]=json_decode($resultado,true);
+    header("Location: view/Layout.php?pagina=rest");
     
-}else{
-    header("Location: ../index.php?pagina=Rest");
 }
+if(isset($_REQUEST["buscar"])){
+ $entrada=true;
+ $aErrores=[];
+ $aErrores[]=validacionFormularios::comprobarNoVacio($_REQUEST["codigo"]);
+}else{
+    $entrada=false;
+}
+if($entrada){
+    $ch= curl_init();
+    curl_setopt($ch,CURLOPT_URL,"https://data.europa.eu/euodp/data/dataset/".$_REQUEST["codigo"]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+    $resultado=curl_exec($ch);
+    curl_close($ch);
+    $_SESSION["resultadoAPI"]=$resultado;
+    header("Location: view/Layout.php?pagina=rest");
+    
+}
+    header("Location: view/Layout.php?pagina=rest");
--- view/vWSRest.php
+++ view/vWSRest.php
@@ -2,11 +2,26 @@
 <h2>Utilización de WS REST Ajenos</h2>
 <p>Información el servicio:</p>
 <form action="../index.php?pagina=rest" method="POST">
-    <label>Parametros de entrada</label><br>
+    <!--<label>Parametros de entrada</label><br>
     <input type="text" name="nombre" id="nombre"><br>
-    <input type="text" name="apellido" id="apellido"><br>
-    <input type="submit" value="Calcular" name="calcular"><br>
+    <input type="text" name="apellido" id="apellido"><br>-->
+    <input type="submit" value="IDS" name="ids"><br>
     <label>Respuesta</label>
+    <p><?php if(isset($_SESSION["resultadoAPI"]["result"])){
+    foreach ($_SESSION["resultadoAPI"]["result"] as $key => $value) {
+        echo $value.","."\n"."<br>";
+    }
+    }
+    ?></p>
+</form>
+<form action="../index.php?pagina=rest" method="POST"> 
+    <label for="codigo">Codigo para la busqueda</label>
+    <input type="text" name="codigo" id="codigo">
+    <input type="submit" value="Buscar" name="buscar">
+    <?php 
+    if(!isset($_SESSION["resultadoAPI"]["result"])){
+    echo $_SESSION["resultadoAPI"];
+    }?>
 </form>
 <a href="../index.php?pagina=inicio"><button>Atras</button></a>
 
